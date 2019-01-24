0009a0bce3a6fbcc648b3f0c7ab22b5feaef237f
unknown <huutam.ptnk@gmail.com>
2019-01-25 00:18:53 +0200

    install beautifulsoup

--- python/main.py
+++ python/main.py
@@ -4,9 +4,11 @@ import requests
 url = "https://www.glassdoor.com/Job/operation-research-analyst-jobs-SRCH_KO0,26_IP3.htm"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'} # This is chrome, you can set whatever browser you like
 
-page = requests.get(url, headers=headers)
-tree = html.fromstring(page.content)
-print(page.content)
+response = requests.get(url, headers=headers)
+# tree = html.fromstring(response.content)
+# data = tree.xpath('//*[@id="MainCol"]/div/ul/li[1]/div[2]/div[1]/div[1]/a')
 
-data = tree.xpath('//*[@id="JobDesc3081549158"]/div/text()[2]')
-print(data)
+print(response.text[:1000])
+html_soup = BeautifulSoup(response.text, 'html.parser')
+
+print(tree.tag)
