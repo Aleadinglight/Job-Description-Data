44f4b7eb7718610d94ce6559fecf9a7300b4ca70
unknown <huutam.ptnk@gmail.com>
2019-01-25 00:42:39 +0200

    just got the result on indeed.com

--- python/main.py
+++ python/main.py
@@ -2,13 +2,13 @@ from lxml import html
 from requests import get
 from bs4 import BeautifulSoup
 
-url = "https://www.indeed.com/q-Job-Find-jobs.html"
+url = "https://www.indeed.com/viewjob?jk=1fedc0d304417c06&tk=1d2105cgi5hhb804&from=serp&vjs=3"
 headers = ({'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
 response = get(url, headers=headers)
 # tree = html.fromstring(response.content)
 # data = tree.xpath('//*[@id="MainCol"]/div/ul/li[1]/div[2]/div[1]/div[1]/a')
-#print(response.text[:100])
+print(response.text[:100])
 
 html_soup = BeautifulSoup(response.text, 'html.parser')
-jobs_desc = html_soup.find_all('span', class_='summary')
+jobs_desc = html_soup.find_all('p')
 print(jobs_desc[:100])
