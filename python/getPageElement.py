087a349116611abfee705f3e11ffb3b0b9848126
unknown <huutam.ptnk@gmail.com>
2019-01-26 17:46:04 +0200

    add a router file

--- python/getPageElement.py
+++ python/getPageElement.py
@@ -2,11 +2,16 @@ from lxml import html
 from requests import get
 from bs4 import BeautifulSoup
 
-url = "https://www.indeed.fi/jobs?q=operations&l=Vaasa"
+url = "https://www.indeed.com/viewjob?jk=1fedc0d304417c06&tk=1d2105cgi5hhb804&from=serp&vjs=3"
 headers = ({'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
-
 response = get(url, headers=headers)
-html_soup = BeautifulSoup(response.text, 'html.parser')
 
-jobs_link  = html_soup.find_all('div',class_ = "jobsearch-SerpJobCard row result clickcard")
-print(jobs_link)
+# print(response.text[:100])
+
+html_soup = BeautifulSoup(response.text, 'html.parser')
+jobs_desc = html_soup.find_all('p')
+
+# Remove all the html tag from the result
+for i in range(0, len(jobs_desc)):
+    print(jobs_desc[i].string)
+
