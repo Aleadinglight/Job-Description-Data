909e6d6c2b60701001b6957d65666f562312d83d
unknown <huutam.ptnk@gmail.com>
2019-01-26 18:11:28 +0200

    added page travel in main.py

--- python/main.py
+++ python/main.py
@@ -2,11 +2,25 @@ from lxml import html
 from requests import get
 from bs4 import BeautifulSoup
 
+web_prefix = "https://www.indeed.fi"
 url = "https://www.indeed.fi/jobs?q=operations&l=Vaasa"
 headers = ({'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
 
 response = get(url, headers=headers)
 html_soup = BeautifulSoup(response.text, 'html.parser')
 
-jobs_link  = html_soup.find_all('div',class_ = "jobsearch-SerpJobCard row result clickcard")
-print(jobs_link)
+# Get the divs that content the jobs
+jobs_link_div  = html_soup.find_all('div',class_ = "jobsearch-SerpJobCard")
+
+# Get the links from the divs
+job_links = []
+for i in range(0, len(jobs_link_div)):
+    job_links.append(jobs_link_div[i].find_all('a')[0]['href'])
+
+#print(job_links[1])
+
+# Travel all the pages:
+for i in range(0, len(job_links)):
+    link = web_prefix + job_links[i]
+    response = get(link, headers=headers)
+    print(response)
